354ee708fcae661c15d12b8bafe0609422c6d387
mostafakamar2308 <moustafa.31011197@med.tanta.edu.eg>
2025-06-14 07:21:42 +0300

    feat: quick changes

--- frontend/src/Components/EditableText.tsx
+++ frontend/src/Components/EditableText.tsx
@@ -36,102 +36,6 @@ const measureTextHeight = (text: string, fontSize: number, padding: number) => {
   return height;
 };
 
-// const TextEditor: React.FC<{
-//   textNode: Konva.Text | null;
-//   onClose: () => void;
-//   onChange: (text: string) => void;
-//   stage: Konva.Stage | null;
-// }> = ({ textNode, onClose, onChange, stage }) => {
-//   const textareaRef = useRef<HTMLTextAreaElement>(null);
-//   const [inputText, setInputText] = useState(textNode?.text() || "");
-//   const [textareaWidth, setTextareaWidth] = useState(200); // Track textarea width
-
-//   useEffect(() => {
-//     const updateTextareaDimensions = () => {
-//       if (!textNode || !stage) return;
-
-//       const newWidth = measureTextWidth(
-//         inputText,
-//         textNode.fontSize(),
-//         textNode.padding() || 0
-//       );
-//       const scaledWidth = newWidth * stage.scaleX();
-
-//       textNode.setAttrs({ width: newWidth });
-//       setTextareaWidth(scaledWidth);
-//     };
-
-//     updateTextareaDimensions();
-//   }, [inputText, textNode, stage]);
-
-//   const computeDOMPosition = useCallback(() => {
-//     if (!textNode || !stage) return { x: 0, y: 0 };
-
-//     const nodeAbsPos = textNode.getAbsolutePosition(); // Get absolute position
-//     const container = stage.container() as HTMLDivElement;
-//     const containerRect = container.getBoundingClientRect();
-//     const scaleX = stage.scaleX();
-//     const scaleY = stage.scaleY();
-//     const stagePos = stage.position();
-
-//     return {
-//       x: containerRect.left + nodeAbsPos.x * scaleX + stagePos.x,
-//       y: containerRect.top + nodeAbsPos.y * scaleY + stagePos.y,
-//     };
-//   }, [textNode, stage]);
-
-//   const { x, y } = computeDOMPosition();
-
-//   useEffect(() => {
-//     const textarea = textareaRef.current;
-//     if (!textarea || !textNode || !stage) return;
-
-//     const handleKeyDown = (e: KeyboardEvent) => {
-//       if (e.key === "Escape") {
-//         setInputText(textNode.text() || ""); // Reset to original text
-//         onClose();
-//       } else if (e.key === "Enter" && !e.shiftKey) {
-//         onChange(inputText);
-//         onClose();
-//       }
-//     };
-
-//     textarea.addEventListener("keydown", handleKeyDown);
-
-//     return () => {
-//       textarea.removeEventListener("keydown", handleKeyDown);
-//     };
-//   }, [textNode, stage, inputText, onChange, onClose]);
-
-//   if (!stage || !textNode) return null;
-
-//   return (
-//     <Html>
-//       <textarea
-//         ref={textareaRef}
-//         value={inputText}
-//         onChange={(e) => setInputText(e.target.value)}
-//         style={{
-//           position: "absolute",
-//           top: `${y}px`,
-//           left: `${x}px`,
-//           width: `${textareaWidth}px`, // Use tracked width
-//           height: `${
-//             textNode.height() * stage.scaleY() + textNode.fontSize()
-//           }px`,
-//           transform: `rotate(${textNode.rotation()}rad)`,
-//           color: textNode.fill().toString(),
-//           fontSize: `${textNode.fontSize()}px`,
-//           border: "1px solid #333",
-//           padding: "2px",
-//           resize: "none", // Disable textarea resizing
-//         }}
-//         autoFocus
-//       />
-//     </Html>
-//   );
-// };
-
 function getStyle(fontSize: number) {
   const isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
   const baseStyle = {
@@ -173,7 +77,6 @@ const TextArea: React.FC<{
     () => measureTextWidth(text, fontSize, 5),
     [text, fontSize]
   );
-  console.log({ width });
 
   useEffect(() => {
     if (textareaRef.current) {
@@ -189,11 +92,14 @@ const TextArea: React.FC<{
       <textarea
         onBlur={() => onBlur && onBlur(text)}
         autoFocus
-        className="resize-none min-w-fit !min-h-fit"
+        className="resize-none min-w-fit"
         value={text}
         ref={textareaRef}
         onChange={handleChange}
-        style={{ ...style, width: width + "px" }}
+        style={{
+          ...style,
+          width: width + "px",
+        }}
       />
     </Html>
   );
